de7894739cc4e08c8b82c78b621c2d71b1b5a77a
alan audivert <alan.audivert@hotmail.fr>
2018-12-20 17:15:14 +0100

    Destroyer V8.0 - Instantiater V8.0
    Destroy and instantiater now supports event raising related to components and childrens. Cancel is also supported for every of these

--- Unity Tools Testing/Assets/test.cs
+++ Unity Tools Testing/Assets/test.cs
@@ -1,5 +1,4 @@
 ﻿using Nalka.Tools.Unity;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,25 +7,37 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    [HideInInspector]
     public string Temp = "Je teste des components";
     private void Start()
     {
         Destroyer.AllowsMultipleEqualHandler = false;
-        Destroyer.Destroying.AddHandler<Test>(Tester);
-        Destroyer.Destroying.AddHandler<GameObject>(GameObject);
+        Destroyer.Destroyed.AddHandler<Object>(e =>
+        {
+            Debug.Log($"Destroyed {e.DestroyedObject.GetType().Name} component from {e.DestroyedObject.name}");
+        });
+        Destroyer.Destroying.AddHandler<Object>(e =>
+        {
+            Debug.Log($"Destroying {e.DestroyedObject.GetType().Name} component from {e.DestroyedObject.name}");
+            if (e.DestroyedObject.name == "Child cube")
+            {
+                Debug.LogWarning($"Cancelled");
+                e.Cancel = true;
+            }
+        });
     }
 
-    private void Update()
+    private async void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Destroyer.Destroy(gameObject).GetAwaiter().GetResult();
+#pragma warning disable CS0618 // Le type ou le membre est obsolète
+            await Destroyer.Destroy(gameObject);
+#pragma warning restore CS0618 // Le type ou le membre est obsolète
         }
         else if (Input.GetKeyDown(KeyCode.B))
         {
             //Instantiater.Test();
         }
     }
-    private void Tester(DestroyingObjectEventArgs<Test> e) => Debug.Log($"Test");
-    private void GameObject(DestroyingObjectEventArgs<GameObject> e) => Debug.Log($"GameObject");
 }
